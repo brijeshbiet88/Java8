7e74bdd288d49c623d48eab64040b696bf09be51
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2020-08-01 18:15:22 +0530

    Random UUIDs Generation

--- src/com/interview/java8/streams/InfiniteStreamDemo.java
+++ src/com/interview/java8/streams/InfiniteStreamDemo.java
@@ -1,6 +1,8 @@
 package com.interview.java8.streams;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -15,6 +17,11 @@ public class InfiniteStreamDemo {
 		infiniteStream = Stream.iterate(2, i -> i * 2);
 		List<Integer> collect2 = infiniteStream.limit(5).collect(Collectors.toList());
 		collect2.forEach(System.out::println);
+	
+		Supplier<UUID> randomUUIDs = UUID :: randomUUID;
+		Stream<UUID> generate = Stream.generate(randomUUIDs);
+		List<UUID> uuidList = generate.skip(5).limit(5).collect(Collectors.toList());
+		uuidList.forEach(System.out::println);
 	}
 
 }
