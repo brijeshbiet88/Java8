56262de0707f91a67340e7db7e5c0a7f978a2421
brijeshbiet88@gmail.com <brijeshbiet88@gmail.com>
2020-08-08 17:39:41 +0530

    UnaryOperator Demo

---
+++ src/com/interview/java8/lambda/UnaryOperatorDemo.java
@@ -0,0 +1,25 @@
+package com.interview.java8.lambda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.UnaryOperator;
+
+public class UnaryOperatorDemo {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(2 , 4 , 6 , 8, 10);
+		UnaryOperator<Integer> unaryOperator = (num)-> num*num;
+		unaryOperatorFun(unaryOperator , list).forEach(s-> System.out.print(s+" "));
+
+	}
+
+	private static List<Integer> unaryOperatorFun(UnaryOperator<Integer> unaryOperator, List<Integer> list) {
+		List<Integer> resultList = new ArrayList<>();
+		list.forEach(element -> resultList.add(unaryOperator.apply(element)));
+		return resultList;
+	}
+
+}
+/*4 16 36 64 100 */
